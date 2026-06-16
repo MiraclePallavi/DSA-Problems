33d525cb6aba680d4663ec2a111cfd892924f29a
Pallavi <157872397+MiraclePallavi@users.noreply.github.com>
2026-06-16 23:39:12 +0530

    Time: 4 ms (63.64%), Space: 39.9 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result = "";
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='*' && !result.empty()){
+                result.pop_back();
+            }
+            else if(s[i]=='#'){
+                result+=result;
+            }
+            else if(s[i]=='%'){
+                reverse(result.begin(), result.end());
+            }
+            else if(islower(s[i])){
+                result+=s[i];
+            }
+        }
+        return result;
+    }
+};
